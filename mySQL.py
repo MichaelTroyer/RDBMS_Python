4214496a43e481d3abf5e69730161890f2151b37
MichaelTroyer <michael.david.troyer@gmail.com>
2019-05-21 21:16:40 -0600

    updated MySQL connect

--- mySQL.py
+++ mySQL.py
@@ -5,23 +5,30 @@ import yaml
 from RDBMSExceptions import ConnectionError
 
 
-def connect(database, host, user, password):
+def connect_to_database(connection_params):
     """
     Establish a database connection and return a connection object.
 
     inputs:
-        database: database name
-        host: server address - defaults to localhost
-        username: defaults to root
-        password: 
-
+        connection_params: dict of connection parameters
+            database: required
+            password: required
+            host: server address - defaults to localhost if not supplied
+            user: username - defaults to root if not supplied
     returns:
         connection object
-
     raises:
         ConnectionError on failure
     """
     try:
+        database = connection_params.pop('database')
+        password = connection_params.pop('password')
+        host = connection_params.pop('host', 'localhost')
+        user = connection_params.pop('user', 'root')
+
+        if connection_params:
+            raise ConnectionError('Unknown keyword(s):', connection_params)
+
         connection = connector.connect(
             database=database,
             host=host,
@@ -30,34 +37,59 @@ def connect(database, host, user, password):
         )
         return connection
 
+    except KeyError as e:
+        raise ConnectionError('Missing connection parameter') from e
     except Exception as e:
-        raise ConnectionError('Error connecting to database:') from e
+        raise ConnectionError('Error connecting to database') from e
 
+
+def create_database(connection, database_params):
+    pass
+
+
+def drop_database(connection, database_name):
+    pass
+
+
+def add_table(connection, table_params):
+    pass
+
+
+def drop_table(connection, table_name):
+    pass
+
+    
+### Maintenance Tasks:
+
+# Backup database with mysqldump
+# Checks table for integrity errors: https://dev.mysql.com/doc/refman/8.0/en/check-table.html
+# Optimize: https://dev.mysql.com/doc/refman/8.0/en/optimize-table.html
+# Analyze: https://dev.mysql.com/doc/refman/8.0/en/analyze-table.html
+
+
+
+### TESTING ###
 
 def test_connection():
     """
-    Test database connectivity by establishing a connection and listing all tables in sample database
-    
-    classicmodels MySQL sample database:
+    Test database connectivity by establishing a connection and listing 
+    all tables in classicmodels MySQL sample database.
     http://www.mysqltutorial.org/mysql-sample-database.aspx
-
-    Read database connection information from config yaml file
-    e.g.
-    host: localhost
-    user: root
-    password: password
-    database: classicmodels
     """
 
+    # Read database connection information from config yaml file
     config_file = r'.\config\MySQL_config.yml'
-    config = yaml.safe_load(open(config_file))
+    config_params = yaml.safe_load(open(config_file))
 
     try:
-        conn = connect(**config)
+        database = config_params['database']
+        conn = connect_to_database(config_params)
+        print('Successfully connected to database: ', database)
         cursor = conn.cursor()
+        print('Tables:')
         cursor.execute("SHOW TABLES")
-        for (table_name,) in cursor:
-                print(table_name)
+        for table_name in cursor:
+                print('\t', table_name[0])
 
     finally:
         try: conn.close()
